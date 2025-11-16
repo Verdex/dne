27dbdbfaea66719ff47e086211e7c897b15c4f36
David L. Inman <davidlevere@gmail.com>
2025-11-16 17:10:13 -0500

    parse input

---
+++ src/parsing/parse_input.rs
@@ -0,0 +1,50 @@
+
+use std::iter::Peekable;
+
+pub struct Input<T, E> {
+    ls : Peekable<std::vec::IntoIter<T>>,
+    eof : E, 
+    fatal : E,
+}
+
+impl<T, E> Input<T, E> {
+    pub fn new(input : Vec<T>, eof : E, fatal : E) -> Self {
+        Input { 
+            ls: input.into_iter().peekable(),
+            eof,
+            fatal,
+        }
+    }
+    pub fn check<F:Fn(&T) -> bool>(&mut self, f : F) -> Result<bool, &E> {
+        match self.ls.peek() {
+            Some(l) if f(l) => {
+                self.ls.next().unwrap();
+                Ok(true)
+            },
+            Some(_) => Ok(false),
+            None => Err(&self.eof),
+        }
+    }
+    pub fn expect<F:Fn(&T) -> bool>(&mut self, f : F) -> Result<T, &E> {
+        match self.ls.peek() {
+            Some(l) if f(l) => {
+                let l = self.ls.next().unwrap();
+                Ok(l)
+            },
+            Some(_) => Err(&self.fatal),
+            None => Err(&self.eof),
+        }
+    }
+    pub fn peek(&mut self) -> Result<&T, &E> {
+        match self.ls.peek() {
+            Some(l) => Ok(l),
+            None => Err(&self.eof),
+        }
+    }
+    pub fn take(&mut self) -> Result<T, &E> {
+        match self.ls.next() {
+            Some(l) => Ok(l),
+            None => Err(&self.eof),
+        }
+    }
+}

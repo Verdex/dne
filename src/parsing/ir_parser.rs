a905b5161a568883f818852d2f287e7bf75bde76
David L. Inman <davidlevere@gmail.com>
2025-11-24 22:30:36 -0500

    inc prog ir parser

--- src/parsing/ir_parser.rs
+++ src/parsing/ir_parser.rs
@@ -1,14 +1,15 @@
 
-use super::lexer::ir;
-use super::parse_input::Input;
+use std::rc::Rc;
+use super::lexer::ir::{self, Token};
 
-#[derive(Debug)]
+type Input = super::parse_input::Input<Token, ParseError>;
+
+#[derive(Debug, Clone)]
 pub enum ParseError {
     Lex(usize),
     Fatal,
     Eof,
 }
-
 
 impl std::fmt::Display for ParseError {
     fn fmt(&self, f : &mut std::fmt::Formatter) -> std::fmt::Result {
@@ -21,11 +22,17 @@ impl std::fmt::Display for ParseError {
 
 impl std::error::Error for ParseError { }
 
-
-
 pub enum Top {
     Global,
     Proc
+}
+
+pub enum Stmt {
+    Set { var: Rc<str>, val: Expr }
+}
+
+pub enum Expr { 
+
 }
 
 pub fn parse(input : &str) -> Result<Vec<Top>, ParseError> {
@@ -38,3 +45,26 @@ pub fn parse(input : &str) -> Result<Vec<Top>, ParseError> {
     todo!()
 }
 
+fn parse_stmts(input : &mut Input) -> Result<Vec<Stmt>, ParseError> {
+    let mut ret = vec![];
+    loop {
+        if input.check(|l| l.eq(&Token::Set))? {
+            ret.push(parse_set(input)?);
+        }
+        else {
+            return Ok(ret);
+        }
+    }
+}
+
+fn parse_set(input : &mut Input) -> Result<Stmt, ParseError> {
+    let var = input.expect(|l| matches!(l, Token::Symbol(_)))?;
+    //input.expect(|l| matches!(l, Token::Equal))?;
+    let val = parse_expr(input)?;
+    //input.expect(|l| l.eq(&Token::SemiColon))?;
+    Ok(Stmt::Set { var: var.value(), val })
+}
+
+fn parse_expr(input : &mut Input) -> Result<Expr, ParseError> {
+    todo!()
+}

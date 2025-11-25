7556ccaf88cdce44453731ac6ecc681d36470384
David L. Inman <davidlevere@gmail.com>
2025-11-24 22:30:23 -0500

    parse_input fixed up

--- src/parsing/parse_input.rs
+++ src/parsing/parse_input.rs
@@ -7,7 +7,7 @@ pub struct Input<T, E> {
     fatal : E,
 }
 
-impl<T, E> Input<T, E> {
+impl<T, E:Clone> Input<T, E> {
     pub fn new(input : Vec<T>, eof : E, fatal : E) -> Self {
         Input { 
             ls: input.into_iter().peekable(),
@@ -15,36 +15,36 @@ impl<T, E> Input<T, E> {
             fatal,
         }
     }
-    pub fn check<F:Fn(&T) -> bool>(&mut self, f : F) -> Result<bool, &E> {
+    pub fn check<F:Fn(&T) -> bool>(&mut self, f : F) -> Result<bool, E> {
         match self.ls.peek() {
             Some(l) if f(l) => {
                 self.ls.next().unwrap();
                 Ok(true)
             },
             Some(_) => Ok(false),
-            None => Err(&self.eof),
+            None => Err(self.eof.clone()),
         }
     }
-    pub fn expect<F:Fn(&T) -> bool>(&mut self, f : F) -> Result<T, &E> {
+    pub fn expect<F:Fn(&T) -> bool>(&mut self, f : F) -> Result<T, E> {
         match self.ls.peek() {
             Some(l) if f(l) => {
                 let l = self.ls.next().unwrap();
                 Ok(l)
             },
-            Some(_) => Err(&self.fatal),
-            None => Err(&self.eof),
+            Some(_) => Err(self.fatal.clone()),
+            None => Err(self.eof.clone()),
         }
     }
-    pub fn peek(&mut self) -> Result<&T, &E> {
+    pub fn peek(&mut self) -> Result<&T, E> {
         match self.ls.peek() {
             Some(l) => Ok(l),
-            None => Err(&self.eof),
+            None => Err(self.eof.clone()),
         }
     }
-    pub fn take(&mut self) -> Result<T, &E> {
+    pub fn take(&mut self) -> Result<T, E> {
         match self.ls.next() {
             Some(l) => Ok(l),
-            None => Err(&self.eof),
+            None => Err(self.eof.clone()),
         }
     }
 }

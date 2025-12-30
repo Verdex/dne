cff78f373ff052c4cfffb6d22f503d17c0fadcb6
David L. Inman <davidlevere@gmail.com>
2025-12-30 13:51:51 -0500

    op tests

--- src/ir_tests/op_tests.rs
+++ src/ir_tests/op_tests.rs
@@ -5,6 +5,124 @@ use crate::eval::data::RuntimeData;
 use super::util::test;
 
 #[test]
+fn should_neg_int() {
+    let input = r"
+proc main() -> Int {
+    set y : Int = 3;
+    set x : Int = call neg_int(y);
+    return x;
+}
+"; 
+
+    let output = proj!(test(input).unwrap(), RuntimeData::Int(x), x);
+    assert_eq!(output, -3);
+}
+
+#[test]
+fn should_neg_float() {
+    let input = r"
+proc main() -> Float {
+    set y : Float = 3.0;
+    set x : Float = call neg_float(y);
+    return x;
+}
+"; 
+
+    let output = proj!(test(input).unwrap(), RuntimeData::Float(x), x);
+    assert_eq!(output, -3.0);
+}
+
+#[test]
+fn should_mod_ints() {
+    let input = r"
+proc main() -> Int {
+    set z : Int = 10;
+    set y : Int = 3;
+    set x : Int = call mod_int(z, y);
+    return x;
+}
+"; 
+
+    let output = proj!(test(input).unwrap(), RuntimeData::Int(x), x);
+    assert_eq!(output, 1);
+}
+
+#[test]
+fn should_mod_floats() {
+    let input = r"
+proc main() -> Float {
+    set z : Float = 10.0;
+    set y : Float = 3.0;
+    set x : Float = call mod_float(z, y);
+    return x;
+}
+"; 
+
+    let output = proj!(test(input).unwrap(), RuntimeData::Float(x), x);
+    assert_eq!(output, 1.0);
+}
+
+#[test]
+fn should_mul_ints() {
+    let input = r"
+proc main() -> Int {
+    set z : Int = 6;
+    set y : Int = 2;
+    set x : Int = call mul_int(z, y);
+    return x;
+}
+"; 
+
+    let output = proj!(test(input).unwrap(), RuntimeData::Int(x), x);
+    assert_eq!(output, 12);
+}
+
+#[test]
+fn should_mul_floats() {
+    let input = r"
+proc main() -> Float {
+    set z : Float = 3.0;
+    set y : Float = 2.0;
+    set x : Float = call mul_float(z, y);
+    return x;
+}
+"; 
+
+    let output = proj!(test(input).unwrap(), RuntimeData::Float(x), x);
+    assert_eq!(output, 6.0);
+}
+
+#[test]
+fn should_div_ints() {
+    let input = r"
+proc main() -> Int {
+    set z : Int = 6;
+    set y : Int = 2;
+    set x : Int = call div_int(z, y);
+    return x;
+}
+"; 
+
+    let output = proj!(test(input).unwrap(), RuntimeData::Int(x), x);
+    assert_eq!(output, 3);
+}
+
+#[test]
+fn should_div_floats() {
+    let input = r"
+proc main() -> Float {
+    set z : Float = 3.0;
+    set y : Float = 2.0;
+    set x : Float = call div_float(z, y);
+    return x;
+}
+"; 
+
+    let output = proj!(test(input).unwrap(), RuntimeData::Float(x), x);
+    assert_eq!(output, 1.5);
+}
+
+#[test]
 fn should_sub_ints() {
     let input = r"
 proc main() -> Int {

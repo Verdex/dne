783d93eb8cbb7463a8e0d6e2e8dac60a22e8be28
David L. Inman <davidlevere@gmail.com>
2025-12-29 22:54:41 -0500

    more tests

--- src/ir_tests.rs
+++ src/ir_tests.rs
@@ -14,6 +14,36 @@ fn test(input : &str) -> Option<RuntimeData> {
 }
 
 #[test]
+fn should_sub_ints() {
+    let input = r"
+proc main() -> Int {
+    set z : Int = 3;
+    set y : Int = 2;
+    set x : Int = call sub_int(z, y);
+    return x;
+}
+"; 
+
+    let output = proj!(test(input).unwrap(), RuntimeData::Int(x), x);
+    assert_eq!(output, 1);
+}
+
+#[test]
+fn should_sub_floats() {
+    let input = r"
+proc main() -> Float {
+    set z : Float = 3.0;
+    set y : Float = 2.0;
+    set x : Float = call sub_float(z, y);
+    return x;
+}
+"; 
+
+    let output = proj!(test(input).unwrap(), RuntimeData::Float(x), x);
+    assert_eq!(output, 1.0);
+}
+
+#[test]
 fn should_add_ints() {
     let input = r"
 proc main() -> Int {

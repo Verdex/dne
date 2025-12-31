b180ba9d2075a351d03b9ddbffd43cdd40afd089
David L. Inman <davidlevere@gmail.com>
2025-12-30 21:49:44 -0500

    more tests

--- src/ir_tests/program_tests.rs
+++ src/ir_tests/program_tests.rs
@@ -4,7 +4,6 @@ use crate::eval::data::RuntimeData;
 
 use super::util::test;
 
-
 #[test]
 fn should_loop_to_100() {
     let input = r"
@@ -26,3 +25,33 @@ proc main() -> Int {
     let output = proj!(test(input).unwrap(), RuntimeData::Int(x), x);
     assert_eq!(output, 100);
 }
+
+#[test]
+fn should_recurse_to_100() {
+    let input = r"
+proc main() -> Int {
+    set start : Int = 4;
+    set x : Int = call to_hundred(start);
+    return x;
+}
+
+proc to_hundred(input : Int) -> Int {
+    set one_hundred : Int = 100;
+    set one : Int = 1;
+
+    set b : Bool = call eq_int(input, one_hundred);
+    set b : Bool = call not(b);
+    branch_true continue b;
+    return input;
+
+    label continue;
+    set input : Int = call add_int(one, input);
+    set output : Int = call to_hundred(input);
+    return output;
+}
+"; 
+
+    let output = proj!(test(input).unwrap(), RuntimeData::Int(x), x);
+    assert_eq!(output, 100);
+}
+

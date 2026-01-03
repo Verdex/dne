0899248c2f9d88cf01a17613792219d7d36a0642
David L. Inman <davidlevere@gmail.com>
2026-01-02 21:51:50 -0500

    get slot test

--- src/ir_tests/cons_tests.rs
+++ src/ir_tests/cons_tests.rs
@@ -5,23 +5,47 @@ use crate::eval::data::RuntimeData;
 use super::util::test;
 
 #[test]
-fn blarg() {
+fn should_get_slot() {
     let input = r"
-proc main() -> Bool {
+proc main() -> Float {
 
     set name : Symbol = ~blah;
-    set p1 : Int = 0;
+    set p1 : Int = 2;
     set p2 : Float = 0.1;
 
-    set w : Ref = cons name (p1, p2);
+    set cell : Ref = cons name (p1, p2);
 
-    delete w;
+    set ret : Float = slot cell 1;
 
-    set ret : Bool = true;
+    delete cell;
+
     return ret;
 }
 "; 
 
-    let output = proj!(test(input).unwrap(), RuntimeData::Bool(x), x);
-    assert_eq!(output, true);
+    let output = proj!(test(input).unwrap(), RuntimeData::Float(x), x);
+    assert_eq!(output, 0.1);
+}
+
+#[test]
+fn blarg() {
+    let input = r"
+proc main() -> Int {
+
+    set name : Symbol = ~blah;
+    set p1 : Int = 2;
+    set p2 : Float = 0.1;
+
+    set w : Ref = cons name (p1, p2);
+
+    set ret : Int = slot w 0;
+
+    delete w;
+
+    return ret;
+}
+"; 
+
+    let output = proj!(test(input).unwrap(), RuntimeData::Int(x), x);
+    assert_eq!(output, 0);
 }

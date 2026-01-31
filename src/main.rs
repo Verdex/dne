6eddccf5fd720434056409484167236aa4061036
David L. Inman <davidlevere@gmail.com>
2026-01-31 18:07:58 -0500

    inc prog

--- src/main.rs
+++ src/main.rs
@@ -7,17 +7,37 @@ mod eval;
 #[cfg(test)]
 mod ir_tests;
 
+use std::fs::File;
+
 fn main() {
 
-    let ir = parsing::ir_parser::parse("").unwrap();
+    let args = std::env::args().into_iter().collect::<Vec<_>>();
 
-    let procs = compiling::ir_compiler::compile(&ir).unwrap();
+    if args.len() <= 1 {
+        println!("usage: dne file+");
+    }
+    else {
 
-    let main = procs.iter().enumerate().find(|(_, x)| *"main" == *x.name ).expect("cannot find main").0;
+        let mut ir = vec![];
+        for path in args {
+            let file = File::open(path).expect("failed to open: {path}");
+            let contents = std::io::read_to_string(file).expect("failure reading: {path}");
+            let mut x = match parsing::ir_parser::parse(&contents) {
+                Ok(x) => x,
+                _ => todo!(),
+            };
+            ir.append(&mut x);
+        }
+        
+        let procs = compiling::ir_compiler::compile(&ir).unwrap();
 
-    let mut vm = eval::vm::Vm::new(procs);
+        let main = procs.iter().enumerate().find(|(_, x)| *"main" == *x.name ).expect("cannot find main").0;
 
-    let result = vm.run(main);
-     
-    println!("{:?}", result);
+        let mut vm = eval::vm::Vm::new(procs);
+
+        let result = vm.run(main);
+         
+        println!("{:?}", result);
+    }
+
 }

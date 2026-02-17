de38606a72e0d693ae01f92b2a977b4922128464
David L. Inman <davidlevere@gmail.com>
2026-02-16 22:17:11 -0500

    unifier inc prog

--- src/compiling/unifier.rs
+++ src/compiling/unifier.rs
@@ -1,4 +1,99 @@
 
+use std::rc::Rc;
+use std::collections::HashMap;
+
+#[derive(Debug)]
+pub enum Term {
+    Var(Rc<str>),
+    Data(Rc<str>, Vec<Term>),
+}
+
+#[derive(Debug)]
+enum Vif<'a> {
+    Value(&'a Term), 
+    Index(usize),
+    Free
+}
+
+#[derive(Debug)]
+pub struct Unify<'a> {
+    vars: Vec<Vif<'a>>,
+    reg: HashMap<Rc<str>, usize>,
+}
+
+impl<'a> Unify<'a> {
+    pub fn new() -> Self { Unify { reg: HashMap::new(), vars: vec![] } } 
+
+    pub fn unify(&mut self, a : &'a Term, b : &'a Term) -> bool {
+        match (a, b) {
+            (Term::Data(a_name, _), Term::Data(b_name, _)) if a_name != b_name => false,
+            (Term::Data(_, a_params), Term::Data(_, b_params)) => 
+                a_params.iter().zip(b_params.iter()).map(|(a, b)| self.unify(a, b)).all(|x| x),
+            (Term::Var(a), Term::Var(b)) => {
+                let a = self.to_index(a);
+                let b = self.to_index(b);
+                let (a_index, a_value) = self.follow(a);
+                let (b_index, b_value) = self.follow(b);
+
+                match (a_value, b_value) {
+                    (None, None) => { self.vars[a_index] = Vif::Index(b_index); true },
+                    (Some(a), Some(b)) => self.unify(a, b),
+                    (None, Some(b)) => { self.vars[a_index] = Vif::Value(b); true },
+                    (Some(a), None) => { self.vars[b_index] = Vif::Value(a); true },
+                }
+            },
+            (Term::Var(a), b) => {
+                let a = self.to_index(a);
+                let (a_index, a_value) = self.follow(a);
+
+                match a_value {
+                    None => { self.vars[a_index] = Vif::Value(b); true },
+                    Some(a) => self.unify(a, b),
+                }
+            },
+            (a, Term::Var(b)) => {
+                let b = self.to_index(b);
+                let (b_index, b_value) = self.follow(b);
+
+                match b_value {
+                    None => { self.vars[b_index] = Vif::Value(a); true },
+                    Some(b) => self.unify(a, b),
+                }
+            },
+        }
+    }
+
+    fn follow(&self, x : usize) -> (usize, Option<&'a Term>) {
+        while let Vif::Index(x) = self.vars[x] { }
+        match self.vars[x] {
+            Vif::Value(v) => (x, Some(v)),
+            Vif::Free => (x, None),
+            Vif::Index(_) => unreachable!(),
+        }
+    }
+
+    fn to_index(&mut self, input : &Rc<str>) -> usize {
+        match self.reg.get(input) {
+            Some(x) => *x,
+            None => {
+                let x = self.vars.len();
+                self.reg.insert(Rc::clone(input), x);   
+                self.vars.push(Vif::Free);
+                x
+            },
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn should_link_free_vars() {
+        
+    }
+}
 
 /*
 #[derive(Debug)]

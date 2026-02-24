2b80ae1b670446ff7fbb43f7773d8bb6c33b9202
David L. Inman <davidlevere@gmail.com>
2026-02-23 21:55:51 -0500

    inc prog

--- src/compiling/dne_static_analysis.rs
+++ src/compiling/dne_static_analysis.rs
@@ -35,30 +35,34 @@ impl<'a> From<&'a Fun> for FunTypeInfo<'a> {
 pub fn static_check(program : &[Fun], built_ins : Vec<FunTypeInfo>) -> Result<(), Vec<StaticError>> {
     let mut fun_names = program.iter().map(|x| Rc::clone(&x.name)).collect::<Vec<_>>();
     check( dup_fun( fun_names.clone() ).into_iter().map(StaticError::DupFunName).collect() )?;
+
     let mut built_in_names = built_ins.iter().map(|x| Rc::clone(&x.name)).collect::<Vec<_>>();
     fun_names.append(&mut built_in_names);
     check( dup_fun( fun_names ).into_iter().map(StaticError::BuiltInCollision).collect() )?;
-    todo!()
+
+    check( types(program, built_ins) )?;
+
+    Ok(())
 }
 
-fn type_check(program : &[Fun], built_ins : Vec<FunTypeInfo>) -> Vec<StaticError> {
+fn types(program : &[Fun], built_ins : Vec<FunTypeInfo>) -> Vec<StaticError> {
 
-
-
-/*
-    let fun_types : HashMap<Rc<str>, TypeInfo> = HashMap::from_iter(
+    let fun_types : HashMap<Rc<str>, FunTypeInfo> = HashMap::from_iter(
         built_ins.into_iter().map(|x| (Rc::clone(&x.name), x))
         .chain(program.iter().map(|x| (Rc::clone(&x.name), x.into()))));
 
-    // TODO look for duplicate fun names or names colliding with built ins
-    //
-    let _ = program.iter().flat_map(|x| check_fun(x, &fun_types));
-    */
-    
-    todo!()
+    program.iter().flat_map(|x| check_fun(x, &fun_types)).collect()
 }
 
 fn check_fun(fun : &Fun, fun_types : &TypeMap) -> Vec<StaticError> {
+    use super::unifier::{Term, Unify};
+
+    fn var(x : &str) -> Term { Term::Var(x.into()) }
+    fn atom(x : &str) -> Term { Term::Data(x.into(), vec![]) }
+    fn rule(x : &str, xs : Vec<Term>) -> Term { Term::Data(x.into(), xs) }
+    
+
+
     // TODO param types
     // TODO let types
     // TODO check calls

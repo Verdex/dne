18f038509c96abe7811b4ec2ec16b9ae232e40e0
David L. Inman <davidlevere@gmail.com>
2026-02-23 21:36:35 -0500

    dup names, etc

--- src/compiling/dne_static_analysis.rs
+++ src/compiling/dne_static_analysis.rs
@@ -4,22 +4,23 @@ use std::collections::{ HashSet, HashMap };
 
 use crate::parsing::dne_parser::*;
 
-type TypeMap<'a> = HashMap<Rc<str>, TypeInfo<'a>>;
+type TypeMap<'a> = HashMap<Rc<str>, FunTypeInfo<'a>>;
 
 pub enum StaticError {
     DupFunName(Rc<str>), 
+    BuiltInCollision(Rc<str>),
 }
 
-pub struct TypeInfo<'a> {
+pub struct FunTypeInfo<'a> {
     name : Rc<str>,
     return_type : &'a Type,
     param_types : Vec<&'a Type>,
     type_params : HashSet<Rc<str>>,
 }
 
-impl<'a> From<&'a Fun> for TypeInfo<'a> {
+impl<'a> From<&'a Fun> for FunTypeInfo<'a> {
     fn from(item: &'a Fun) -> Self {
-        TypeInfo { 
+        FunTypeInfo { 
             name: Rc::clone(&item.name),
             param_types: item.params.iter().map(|(_, t)| t).collect(),
             return_type: &item.return_type,
@@ -28,7 +29,20 @@ impl<'a> From<&'a Fun> for TypeInfo<'a> {
     }
 }
 
-pub fn type_check(program : &[Fun], built_ins : Vec<TypeInfo>) -> Vec<StaticError> {
+pub fn static_check(program : &[Fun], built_ins : Vec<FunTypeInfo>) -> Result<(), Vec<StaticError>> {
+    let mut fun_names = program.iter().map(|x| Rc::clone(&x.name)).collect::<Vec<_>>();
+    check( dup_fun( fun_names.clone() ).into_iter().map(StaticError::DupFunName).collect() )?;
+    let mut built_in_names = built_ins.iter().map(|x| Rc::clone(&x.name)).collect::<Vec<_>>();
+    fun_names.append(&mut built_in_names);
+    check( dup_fun( fun_names ).into_iter().map(StaticError::BuiltInCollision).collect() )?;
+    todo!()
+}
+
+fn type_check(program : &[Fun], built_ins : Vec<FunTypeInfo>) -> Vec<StaticError> {
+
+
+
+/*
     let fun_types : HashMap<Rc<str>, TypeInfo> = HashMap::from_iter(
         built_ins.into_iter().map(|x| (Rc::clone(&x.name), x))
         .chain(program.iter().map(|x| (Rc::clone(&x.name), x.into()))));
@@ -36,6 +50,7 @@ pub fn type_check(program : &[Fun], built_ins : Vec<TypeInfo>) -> Vec<StaticErro
     // TODO look for duplicate fun names or names colliding with built ins
     //
     let _ = program.iter().flat_map(|x| check_fun(x, &fun_types));
+    */
     
     todo!()
 }
@@ -47,4 +62,14 @@ fn check_fun(fun : &Fun, fun_types : &TypeMap) -> Vec<StaticError> {
     todo!()
 }
 
+fn dup_fun(mut x : Vec<Rc<str>>) -> Vec<Rc<str>> {
+    x.sort(); 
+    let mut x = x.iter().zip(x.iter().skip(1)).filter_map(|(a, b)| if a == b { Some(Rc::clone(a)) } else { None }).collect::<Vec<_>>();
+    x.dedup();
+    x
+}
 
+fn check(x : Vec<StaticError>) -> Result<(), Vec<StaticError>> {
+    if x.len() == 0 { Ok(()) }
+    else { Err(x) }
+}

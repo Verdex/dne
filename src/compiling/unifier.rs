e48cc45e85fdd8be85dfd20ea413087f2131f74a
David L. Inman <davidlevere@gmail.com>
2026-02-28 00:22:27 -0500

    have unifier use RC instead of lifetime

--- src/compiling/unifier.rs
+++ src/compiling/unifier.rs
@@ -5,29 +5,29 @@ use std::collections::HashMap;
 #[derive(Debug)]
 pub enum Term {
     Var(Rc<str>),
-    Data(Rc<str>, Vec<Term>),
+    Data(Rc<str>, Vec<Rc<Term>>),
 }
 
 #[derive(Debug)]
-enum Vif<'a> {
-    Value(&'a Term), 
+enum Vif {
+    Value(Rc<Term>), 
     Index(usize),
     Free
 }
 
 #[derive(Debug)]
-pub struct Unify<'a> {
-    vars: Vec<Vif<'a>>,
+pub struct Unify {
+    vars: Vec<Vif>,
     reg: HashMap<Rc<str>, usize>,
 }
 
-impl<'a> Unify<'a> {
+impl Unify {
     pub fn new() -> Self { Unify { reg: HashMap::new(), vars: vec![] } } 
 
-    pub fn env(&self) -> Vec<(Rc<str>, &'a Term)> {
+    pub fn env(&self) -> Vec<(Rc<str>, Rc<Term>)> {
         self.reg.iter().filter_map(|(k, v)| {
             match &self.vars[*v] {
-                Vif::Value(x) => Some((Rc::clone(k), *x)),
+                Vif::Value(x) => Some((Rc::clone(k), Rc::clone(x))),
                 Vif::Index(x) => {
                     match self.follow(*x).1 {
                         None => None,
@@ -40,49 +40,51 @@ impl<'a> Unify<'a> {
     }
 
 // TODO occurs
-    pub fn unify(&mut self, a : &'a Term, b : &'a Term) -> bool {
-        match (a, b) {
+    pub fn unify(&mut self, a : &Rc<Term>, b : &Rc<Term>) -> bool {
+        let a = Rc::clone(a);
+        let b = Rc::clone(b);
+        match (&*a, &*b) {
             (Term::Data(a_name, _), Term::Data(b_name, _)) if a_name != b_name => false,
             (Term::Data(_, a_params), Term::Data(_, b_params)) => 
                 a_params.iter().zip(b_params.iter()).map(|(a, b)| self.unify(a, b)).all(|x| x),
             (Term::Var(a), Term::Var(b)) => {
-                let a = self.to_index(a);
-                let b = self.to_index(b);
+                let a = self.to_index(&a);
+                let b = self.to_index(&b);
                 let (a_index, a_value) = self.follow(a);
                 let (b_index, b_value) = self.follow(b);
 
                 match (a_value, b_value) {
                     (None, None) => { self.vars[a_index] = Vif::Index(b_index); true },
-                    (Some(a), Some(b)) => self.unify(a, b),
+                    (Some(a), Some(b)) => self.unify(&a, &b),
                     (None, Some(b)) => { self.vars[a_index] = Vif::Value(b); true },
                     (Some(a), None) => { self.vars[b_index] = Vif::Value(a); true },
                 }
             },
-            (Term::Var(a), b) => {
+            (Term::Var(a), _) => {
                 let a = self.to_index(a);
                 let (a_index, a_value) = self.follow(a);
 
                 match a_value {
                     None => { self.vars[a_index] = Vif::Value(b); true },
-                    Some(a) => self.unify(a, b),
+                    Some(a) => self.unify(&a, &b),
                 }
             },
-            (a, Term::Var(b)) => {
+            (_, Term::Var(b)) => {
                 let b = self.to_index(b);
                 let (b_index, b_value) = self.follow(b);
 
                 match b_value {
                     None => { self.vars[b_index] = Vif::Value(a); true },
-                    Some(b) => self.unify(a, b),
+                    Some(b) => self.unify(&a, &b),
                 }
             },
         }
     }
 
-    fn follow(&self, mut x : usize) -> (usize, Option<&'a Term>) {
+    fn follow(&self, mut x : usize) -> (usize, Option<Rc<Term>>) {
         while let Vif::Index(y) = self.vars[x] { x = y; }
         match self.vars[x] {
-            Vif::Value(v) => (x, Some(v)),
+            Vif::Value(ref v) => (x, Some(Rc::clone(v))),
             Vif::Free => (x, None),
             Vif::Index(_) => unreachable!(),
         }

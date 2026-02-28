9a045eb1f56bbb9f89779dc635915b060f48b010
David L. Inman <davidlevere@gmail.com>
2026-02-27 21:01:19 -0500

    inc prog

--- src/compiling/dne_static_analysis.rs
+++ src/compiling/dne_static_analysis.rs
@@ -12,6 +12,7 @@ type TypeMap<'a> = HashMap<Rc<str>, FunTypeInfo<'a>>;
 pub enum StaticError {
     DupFunName(Rc<str>), 
     BuiltInCollision(Rc<str>),
+    ExprIllTyped { expr: Rc<str>, found_type : Rc<str>, expected_type : Rc<str> },
     // TODO dup type names
     // TODO type name collides with built in
     // TODO type parameter shadows outer type (maybe a problem?)
@@ -52,25 +53,29 @@ pub fn static_check(program : &[Fun], built_ins : Vec<FunTypeInfo>) -> Result<()
 
 fn types(program : &[Fun], built_ins : Vec<FunTypeInfo>) -> Vec<StaticError> {
 
+    fn f(x : Result<(), Vec<StaticError>>) -> Vec<StaticError> { match x { Ok(_) => vec![], Err(x) => x } }
+
     let fun_types : HashMap<Rc<str>, FunTypeInfo> = HashMap::from_iter(
         built_ins.into_iter().map(|x| (Rc::clone(&x.name), x))
         .chain(program.iter().map(|x| (Rc::clone(&x.name), x.into()))));
 
-    program.iter().flat_map(|x| check_fun(x, &fun_types)).collect()
+    program.iter().map(|x| check_fun(x, &fun_types)).flat_map(f).collect()
 }
 
-fn check_fun(target : &Fun, global_fun_types : &TypeMap) -> Vec<StaticError> {
+fn check_fun(target : &Fun, global_fun_types : &TypeMap) -> Result<(), Vec<StaticError>> {
 
     // TODO rename
     fn var(x : &Rc<str>) -> Term { Term::Var(Rc::clone(x)) }
     fn atom(x : &Rc<str>) -> Term { Term::Data(Rc::clone(x), vec![]) }
     fn rule(x : &Rc<str>, xs : Vec<Term>) -> Term { Term::Data(Rc::clone(x), xs) }
     
-    let mut unifier = Unify::new();
+    let mut checker = Checker::new();
 
-    let mut ts : HashMap<Rc<str>, Term> = 
-        HashMap::from_iter(target.params.iter().map(|(n, t)| (Rc::clone(n), type_to_term(t, &HashSet::new()))));
-    
+    for (n, t) in &target.params {
+        checker.type_var(n, type_to_term(&t, &HashSet::new()));
+    }
+
+    check(expr(&mut checker, &target.expr, type_to_term(&target.return_type, &HashSet::new())))?;
 
     // TODO build up hashmap with variable to type (which i think is only internal to expr type)
     // Note target's param types don't really matter because they can be considered as some type X
@@ -85,13 +90,27 @@ fn check_fun(target : &Fun, global_fun_types : &TypeMap) -> Vec<StaticError> {
     todo!()
 }
 
-// TODO will have ts and global fun types as inputs
-// TODO i think can have it's own unifier because when we're done we need a real type
-// TODO will need the return type (fun x<T>() -> Fun<T, T> { fun a<S>( x : S ) -> S { x } a }
-// that example unifies 'a' that has effectively forall s . s -> s into T -> T
-fn check_expr(expr : Expr) -> Result<Type, Vec<StaticError>> { 
-
-    todo!()
+fn expr(checker : &mut Checker, expr : &Expr, expected_type : Term) -> Vec<StaticError> { 
+    let t = match expr {
+        Expr::Lit(Lit::Int(_)) => Term::Data("Int".into(), vec![]),
+        Expr::Lit(Lit::Float(_)) => Term::Data("Float".into(), vec![]),
+        Expr::Lit(Lit::Bool(_)) => Term::Data("Bool".into(), vec![]),
+        Expr::Lit(Lit::String(_)) => Term::Data("String".into(), vec![]),
+        _ => todo!(),
+    };
+    // TODO:  This seems unfortunate
+    let error_expected_type : Rc<str> = format!("{:?}", expected_type).into();
+    let error_found_type : Rc<str> = format!("{:?}", t).into();
+    if !checker.unify_types(t, expected_type) {
+        vec![ StaticError::ExprIllTyped { 
+            expr: format!("{:?}", expr).into(), 
+            found_type: error_found_type,
+            expected_type: error_expected_type,
+        }]
+    }
+    else {
+        vec![]
+    }
 }
 
 fn type_to_term(t : &Type, vars : &HashSet<Rc<str>>) -> Term {
@@ -113,4 +132,25 @@ fn dup_fun(mut x : Vec<Rc<str>>) -> Vec<Rc<str>> {
 fn check(x : Vec<StaticError>) -> Result<(), Vec<StaticError>> {
     if x.len() == 0 { Ok(()) }
     else { Err(x) }
+}
+
+struct Checker<'a> {
+    unifier : Unify<'a>,
+    vars : HashMap<Rc<str>, Term>,
+}
+
+impl<'a> Checker<'a> {
+    pub fn new() -> Self { Checker { unifier: Unify::new(), vars: HashMap::new() } }
+
+    pub fn infer_var(&mut self, var : &Rc<str>) {
+        todo!()
+    }
+
+    pub fn type_var(&mut self, var : &Rc<str>, t : Term) {
+        todo!()
+    }
+
+    pub fn unify_types(&mut self, a : Term, b : Term) -> bool {
+        todo!()
+    }
 }

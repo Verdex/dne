8aad4fd6da593cd18587245e54103b6522a0d478
David L. Inman <davidlevere@gmail.com>
2026-03-28 19:11:47 -0400

    zero or more and tests

--- src/parsing/dne_parser.rs
+++ src/parsing/dne_parser.rs
@@ -426,8 +426,28 @@ fn expect_sym(input : &mut Input) -> Result<Rc<str>, ParseError> {
     }
 }
 
-// empty_one_or_more
-// zom
+fn zero_or_more<T>(
+    input : &mut Input, 
+    end : Token, 
+    item : impl Fn(&mut Input) -> Result<T, ParseError>,
+    trail : bool) -> Result<Vec<T>, ParseError> {
+
+    if input.check(|x| x.eq(&end))? {
+        return Ok(vec![]);
+    }
+    let mut xs = vec![];
+    xs.push(item(input)?);
+    loop { 
+        if input.check(|x| x.eq(&end))? {
+            return Ok(xs);
+        }
+        input.expect(|x| x.eq(&Token::Comma))?;
+        if trail && input.check(|x| x.eq(&end))? {
+            return Ok(xs);
+        }
+        xs.push(item(input)?);
+    }
+}
 
 fn one_or_more<T>(
     input : &mut Input, 
@@ -452,16 +472,48 @@ fn one_or_more<T>(
 mod test {
     use super::*;
 
-    fn test_one_or_more(input : &str, trail : bool, success : bool) {
+    fn test_zero_or_more(input : &str, trail : bool, success : bool) {
         let input = dne::lex(input).unwrap();
         let mut input = Input::new(input, ParseError::Eof, |s, e| ParseError::Fatal(s, e));
-        let out = one_or_more(&mut input, Token::RCurl, |x| x.expect(|x| x.eq(&Token::Let)), trail);
+        let out = zero_or_more(&mut input, Token::RCurl, |x| x.expect(|x| x.eq(&Token::Let)), trail);
         if success {
             assert!(matches!(out, Ok(_)));
         }
         else {
             assert!(matches!(out, Err(_)));
         }
+    }
+
+    fn test_one_or_more(target: &str, trail : bool, success : bool) {
+        let input = dne::lex(target).unwrap();
+        let mut input = Input::new(input, ParseError::Eof, |s, e| ParseError::Fatal(s, e));
+        let out = one_or_more(&mut input, Token::RCurl, |x| x.expect(|x| x.eq(&Token::Let)), trail);
+        if success {
+            assert!(matches!(out, Ok(_)), "{}", target);
+        }
+        else {
+            assert!(matches!(out, Err(_)), "{}", target);
+        }
+    }
+
+    #[test]
+    fn should_handle_zero_or_more() {
+        test_zero_or_more("}", false, true);
+        test_zero_or_more("let }", false, true);
+        test_zero_or_more("let, let }", false, true);
+        test_zero_or_more("let, let, let }", false, true);
+        test_zero_or_more("let, let, let, }", false, false);
+        test_zero_or_more("let, let,, let }", false, false);
+        test_zero_or_more("}", true, true);
+        test_zero_or_more("let }", true, true);
+        test_zero_or_more("let, let }", true, true);
+        test_zero_or_more("let, let, let }", true, true);
+        test_zero_or_more("let, let, let, }", true, true);
+        test_zero_or_more("let, let,, let }", true, false);
+        test_zero_or_more("let, }", true, true);
+        test_zero_or_more("let, let, }", true, true);
+        test_zero_or_more("let, let, let, }", true, true);
+        test_zero_or_more("let, let,, let, }", true, false);
     }
 
     #[test]
